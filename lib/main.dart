8af077f73225f51e3cf04b7c6525caa9e6603408
Hadences <jsnpk1213@gmail.com>
2023-09-15 18:01:48 -0500

    Added the favorites page

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         page = GeneratorPage();
         break;
       case 1:
-        page = Placeholder();
+        page = FavoritesPage();
         break;
       default:
         throw UnimplementedError('no widget for $selectedIndex');
@@ -102,6 +102,35 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       );
     });
+  }
+}
+
+class FavoritesPage extends StatelessWidget {
+  const FavoritesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+        child: Text("No current favorites!"),
+      );
+    }
+
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(20),
+          child: Text("You have ${appState.favorites.length} favorites!"),
+        ),
+        for (var pair in appState.favorites)
+          ListTile(
+            title: Text(pair.asPascalCase),
+            leading: Icon(Icons.favorite),
+          ),
+      ],
+    );
   }
 }
 
